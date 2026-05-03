4c3984551d6bdcd54971c51e2628983973a9d17b
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:28:35 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# iot-temperature-monitor
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("IoT Temperature Monitor using simulated sensor data in Python")
+    print("Project: iot-temperature-monitor")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
